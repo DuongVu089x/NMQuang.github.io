53e7798dcc1468c7960c8258efa0eb23e30e0150
Minh Quang <minhquangnguyen1996@gmail.com>
2017-03-03 15:23:10 +0700

    upload file html-css

---
+++ HTML-CSS/Web5/js/style.js
@@ -0,0 +1,10 @@
+﻿function hideContent(showhideDiv){
+	var div = document.getElementById(showhideDiv);
+	if(div.style.display == "block"){
+		div.style.display = "none";
+		
+	}else{
+		div.style.display = "block";
+		
+	}
+}
