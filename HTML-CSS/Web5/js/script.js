db309a7b26037d7f682ac6903dfcb0d939ab629c
Minh Quang <minhquangnguyen1996@gmail.com>
2017-03-06 11:09:38 +0700

    sua file script

---
+++ HTML-CSS/Web5/js/script.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function() {
+	var index = 1;
+	showDiv(index);
+	/*
+	show div content
+	@param {int} n position of div
+	*/
+	function showDiv(n) {
+		$("#film_" + n).show();
+		$("#icon_" +n).removeClass("sub_active");
+	}
+	/*
+	hide div content
+	@param {int} n position of div
+	*/
+	function hideDiv(n) {
+		$("#film_" + n).hide();
+		$("#icon_" +n).addClass("sub_active");
+	}
+
+	jQuery.fn.onClick = function(n) {
+		hideDiv(index);
+		showDiv(n);
+		index = n;
+	}
+	jQuery.fn.ondbClick = function(n) {
+		hideDiv(n);
+	}
+});
