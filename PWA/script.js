239034d8fa7f579b9021da72489ec31c801d9cc8
Minh Quang <minhquangnguyen1996@gmail.com>
2017-04-11 13:43:40 +0700

    fix file sw and script

--- PWA/script.js
+++ PWA/script.js
@@ -1,7 +1,7 @@
  (function() {
  	// TODO add service worker code here
 	if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('../service-worker.js').then(function (registration) {
+        navigator.serviceWorker.register('service-worker.js').then(function (registration) {
             // Registration was successful 
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
 
